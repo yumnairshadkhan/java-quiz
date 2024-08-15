46ecb6e23adef8fbdc1180feb3b9c9a90b30c60e
Your Name <your_email@example.com>
2024-08-15 23:43:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+
+
+
+
+startBtn.onclick =() =>{
+    popupInfo.classList.add('active');
+}
+exitBtn.onclick =() =>{
+    popupInfo.classList.remove('active');
+}
+
+
+
